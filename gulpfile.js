2b9df8bb9b90d668e5f7df7078878832b788cd7b
Christopher Okhravi <christopher.okhravi@gmail.com>
2014-04-01 13:06:32 +0200

    Output to console when compiling jade files

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require('gulp'),
 
 gulp.task('default', function(){
   gulp.watch('src/**/*.jade', function(e){
+    console.log("Running jade on:  " + e.path);
     gulp.src(e.path)
       .pipe(jade())
       .pipe(gulp.dest('./dist'));
