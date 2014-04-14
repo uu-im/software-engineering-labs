2bee238b4bb2ec5dbd3b97f8b5a4393b29c7cb43
Christopher Okhravi <christopher.okhravi@gmail.com>
2014-04-14 21:02:50 +0200

    Add shell project for FizzBuzzer TAD strategy, in lab 2

---
+++ code/block2/FizzBuzzer/FizzBuzzTAD/FizzBuzz.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FizzBuzzTAD
+{
+    class FizzBuzz
+    {
+        public static string FizzOrBuzz(int n)
+        {
+            // returns "Fizz" if divisible by 3
+            // returns "Buzz" if divisible by 5
+            // returns "FizzBuzz" if divisible by 3 and 5
+            throw new NotImplementedException();
+        }
+    }
+}
