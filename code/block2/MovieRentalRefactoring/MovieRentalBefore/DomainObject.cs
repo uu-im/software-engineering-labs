05e0fc4b3fceb7cb73c28fb4a7d7d727a7d204cc
Christopher Okhravi <christopher.okhravi@gmail.com>
2014-04-14 19:35:28 +0200

    Rewrite starting point project for refactoring exercise in lab 2

---
+++ code/block2/MovieRentalRefactoring/MovieRentalBefore/DomainObject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MovieRentalBefore
+{
+    class DomainObject
+    {
+        public string Name { get; protected set; }
+
+        public DomainObject(string name)
+	    {
+            Name = name;
+	    }
+
+	    public DomainObject()
+	    {
+            Name = "No name";
+	    }
+
+        public override string ToString()
+        {
+ 	         return Name;
+        }
+    }
+}
---
+++ code/block2/MovieRentalRefactoring/MovieRentalBefore/Rental.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MovieRentalBefore
+{
+    class Rental : DomainObject
+    {
+        public int DaysRented { get; private set; }
+        public DVD DVD { get; private set; }
+
+        public Rental(DVD dvd, int daysRented) {
+    	    DVD = dvd;
+    	    DaysRented = daysRented;
+        }
+    }
+}
