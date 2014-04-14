1c90fd738d41d0dc3e7cf3216f28134c34579540
Christopher Okhravi <christopher.okhravi@gmail.com>
2014-04-14 21:08:59 +0200

    Extra clarity in code comment requirements

--- code/block2/FizzBuzzer/FizzBuzzTAD/FizzBuzz.cs
+++ code/block2/FizzBuzzer/FizzBuzzTAD/FizzBuzz.cs
@@ -9,6 +9,7 @@ namespace FizzBuzzTAD
             // returns "Fizz" if divisible by 3
             // returns "Buzz" if divisible by 5
             // returns "FizzBuzz" if divisible by 3 and 5
+            // returns the number itself in all other cases
             throw new NotImplementedException();
         }
     }
