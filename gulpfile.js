15f9108effbde5ded181dd6084caa2aff88b1d39
chrokh <christopher.okhravi@gmail.com>
2015-03-13 11:17:30 +0100

    Separate development from production so that I'm not checking in the dist all the time

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,18 @@ var gulp = require('gulp'),
 gulp.task('default', function(){
   var watchfiles = ['src/**/*.*', 'assets/**/*.*']
   gulp.watch(watchfiles, function(e){
-    console.log('Running jade');
+    console.log('Changed: ' + e.path);
     gulp.src('./src/*.jade')
       .pipe(jade().on('error', function(error){
         console.log('Jade error: ', error);
       }))
-      .pipe(gulp.dest('./dist'));
+      .pipe(gulp.dest('./www'));
   });
+});
+
+
+gulp.task('build', function(){
+  gulp.src('./src/*.jade')
+    .pipe(jade())
+    .pipe(gulp.dest('./dist'));
 });
