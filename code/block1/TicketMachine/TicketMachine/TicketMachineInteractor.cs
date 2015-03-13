14cb2cb2e56245d5808de566417aa06871cb00e6
chrokh <christopher.okhravi@gmail.com>
2015-03-13 16:12:52 +0100

    Minor refactoring of interactor

--- code/block1/TicketMachine/TicketMachine/TicketMachineInteractor.cs
+++ code/block1/TicketMachine/TicketMachine/TicketMachineInteractor.cs
@@ -5,76 +5,128 @@ using System.Text;
 
 namespace TicketMachine
 {
-    class TicketMachineInteractor
+  class TicketMachineInteractor
+  {
+    ITicketMachine tm;
+
+    public TicketMachineInteractor(ITicketMachine ticketMachine)
     {
-        private ITicketMachine _ticketMachine;
-
-        public TicketMachineInteractor(ITicketMachine ticketMachine)
-        {
-            _ticketMachine = ticketMachine;
-        }
-
-        public void Start()
-        {
-            showMenu();
-            do
-            {
-                Console.WriteLine("\r\n ==============================================");
-                Console.WriteLine(" Choose action (h for help)");
-            } while (handleInput(read()));
-        }
-
-        private void write(string msg)
-        {
-            Console.WriteLine(" >>> " + msg);
-        }
-
-        private string read()
-        {
-            Console.Write(" >   ");
-            return Console.ReadLine();
-        }
-
-        private bool handleInput(string input)
-        {
-            switch (input)
-            {
-                case "input": case "i":
-                    write("Enter the amount you wish to insert (SEK): ");
-                    int amount;
-                    while (!Int32.TryParse(read(), out amount))
-                        write("Only positive integers, try again:");
-                    write(_ticketMachine.InsertMoney(amount));
-                    return true;
-                case "balance": case "b":
-                    write(_ticketMachine.ShowBalance());
-                    return true;
-                case "refund": case "r":
-                    write(_ticketMachine.RefundMoney());
-                    return true;
-                case "ticket": case "t":
-                    write(_ticketMachine.PrintTicket());
-                    return true;
-                case "p": case "price":
-                    write(_ticketMachine.ShowPrice());
-                    return true;
-                case "q": case "quit":
-                    return false;
-                default:
-                    showMenu();
-                    return true;
-            }
-        }
-
-        private void showMenu()
-        {
-            Console.WriteLine("");
-            Console.WriteLine(" (q) quit\t: Quit");
-            Console.WriteLine(" (i) insert \t: Insert money");
-            Console.WriteLine(" (b) balance \t: Show balance");
-            Console.WriteLine(" (p) price \t: Show price");
-            Console.WriteLine(" (r) refund \t: Refund money");
-            Console.WriteLine(" (t) ticket \t: Print ticket");
-        }
+      tm = ticketMachine;
     }
+
+    public void Start()
+    {
+      UserInteractor.Clear();
+      Console.WriteLine(" (q) quit\t: Quit");
+      Console.WriteLine(" (i) insert \t: Insert money");
+      Console.WriteLine(" (b) balance \t: Show balance");
+      Console.WriteLine(" (p) price \t: Show price");
+      Console.WriteLine(" (r) refund \t: Refund money");
+      Console.WriteLine(" (t) ticket \t: Print ticket");
+      act();
+    }
+
+    private void act()
+    {
+      string result = "";
+      switch(UserInteractor.Ask("Choose action: ")){
+
+        case "q":
+        case "quit":
+        UserInteractor.Confirm();
+        return;
+
+        case "i":
+        case "insert":
+        result = insertMoney();
+        break;
+
+        case "b":
+        case "balance":
+        result = showBalance();
+        break;
+
+        case "r":
+        case "refund":
+        result = refundMoney();
+        break;
+
+        case "t":
+        case "ticket":
+        result = printTicket();
+        break;
+
+        case "p":
+        case "price":
+        result = showPrice();
+        break;
+
+        default:
+          result = "Please enter a valid option.";
+        break;
+      }
+
+      UserInteractor.Say(result);
+      UserInteractor.Confirm();
+      Start();
+    }
+
+
+    public string insertMoney()
+    {
+      int amount;
+      while (!Int32.TryParse(UserInteractor.Ask("Please enter amount: "), out amount))
+      {
+        UserInteractor.Say("Only positive integers, try again: ");
+      }
+
+      return tm.InsertMoney(amount);
+    }
+
+    public string showBalance()
+    {
+      return tm.ShowBalance();
+    }
+
+    public string showPrice()
+    {
+      return tm.ShowPrice();
+    }
+
+    public string refundMoney()
+    {
+      return tm.RefundMoney();
+    }
+
+    public string printTicket()
+    {
+      return tm.PrintTicket();
+    }
+  }
+
+
+
+
+
+
+  class UserInteractor
+  {
+    public static void Say(string message){
+      Console.WriteLine(" >>> " + message);
+    }
+
+    public static string Ask(string message){
+      Console.Write(" >   " + message);
+      return Console.ReadLine();
+    }
+
+    public static void Confirm(){
+      Console.WriteLine(" >   Press any key...");
+      Console.ReadKey();
+    }
+
+    public static void Clear(){
+      Console.Clear();
+    }
+  }
 }
