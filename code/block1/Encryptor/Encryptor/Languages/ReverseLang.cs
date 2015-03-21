63cbfa2149188da80aeaa5a67d8f615fedf71389
chrokh <christopher.okhravi@gmail.com>
2015-03-21 22:28:47 +0100

    Add reverse lang

---
+++ code/block1/Encryptor/Encryptor/Languages/ReverseLang.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Encryptor
+{
+  class ReverseLang : ILanguage
+  {
+    public string Encrypt(string input)
+    {
+      var chars = input
+        .ToCharArray()
+        .OfType<char>()
+        .ToList();
+
+      chars.Reverse();
+
+      return new String(chars.ToArray());
+    }
+
+    public string Decrypt(string input)
+    {
+      return Encrypt(input);
+    }
+
+    public string GetName()
+    {
+      return "Reverser";
+    }
+  }
+}
