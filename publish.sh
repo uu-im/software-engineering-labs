55e7d4f2dd7863ab6d14541a17d1796a84e0e56a
chrokh <christopher.okhravi@gmail.com>
2015-05-08 09:20:06 +0200

    Push all branches when publishing

--- publish.sh
+++ publish.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gulp p && git add -A && git commit -m "Rebuild dist" && git checkout gh-pages && git merge --no-ff -m "Merging 'master' into 'gh-pages'" master && git checkout master && git push
+gulp p && git add -A && git commit -m "Rebuild dist" && git checkout gh-pages && git merge --no-ff -m "Merging 'master' into 'gh-pages'" master && git checkout master && git push --all
