b4ecfff04b14e9fbdda47517d0a9f0cd15bfd653
chrokh <christopher.okhravi@gmail.com>
2017-04-02 22:53:01 +0200

    Serious revamp of the study material page

--- src/vt17/assets/js/main.js
+++ src/vt17/assets/js/main.js
@@ -4,17 +4,17 @@ $(function(){
     hide: '&#9660;'
   }
 
-  $('.hideable').append(' <span></span>')
+  $('.hideable').append(' <span class="hideable-icon"></span>')
     .attr('href', '#')
-    .find('span').html(symbols.hide);
+    .find('span.hideable-icon').html(symbols.hide);
   $('.hideable').click(function(e){
     var target = $(this).attr('data-target');
     var $e = $('#' + target);
     if($e.is(':visible')){
-      $(this).find('span').html(symbols.show);
+      $(this).find('span.hideable-icon').html(symbols.show);
       $e.hide();
     }else{
-      $(this).find('span').html(symbols.hide);
+      $(this).find('span.hideable-icon').html(symbols.hide);
       $e.show().addClass('flash');
     }
     e.preventDefault();
