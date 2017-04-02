03509222defa9ea711f6b161966790c80c2fba5d
chrokh <christopher.okhravi@gmail.com>
2017-04-02 23:38:41 +0200

    Simplify publish script by assuming latest build version as been commited

--- publish.sh
+++ publish.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-source ./build.sh &&
-  git add -A &&
-  git commit -m "Rebuild dist" &&
-  git checkout gh-pages &&
+git checkout gh-pages &&
   git merge --no-ff -m "Merging 'master' into 'gh-pages'" master &&
   git push origin gh-pages &&
   git checkout master
