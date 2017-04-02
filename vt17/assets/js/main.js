530bc77bdb0ce432f1c5a756d91a6904d4fd7f0d
chrokh <christopher.okhravi@gmail.com>
2017-04-02 23:37:38 +0200

    Rebuild

--- vt17/assets/js/main.js
+++ vt17/assets/js/main.js
@@ -1,24 +1,40 @@
-$(function(){
+var lazyLoadIframe = function($container) {
+  $container.find('iframe').each(function(n, iframe) {
+    if ($(iframe).attr('src') != undefined) {
+      return
+    } else {
+      $(iframe).attr('src', $(iframe).attr('data-src'))
+    }
+  })
+}
+
+var initHideables = function() {
   var symbols = {
     show: '', //&#9654;
     hide: '&#9660;'
   }
 
-  $('.hideable').append(' <span></span>')
+  $('.hideable').append(' <span class="hideable-icon"></span>')
     .attr('href', '#')
-    .find('span').html(symbols.hide);
+    .find('span.hideable-icon').html(symbols.hide);
   $('.hideable').click(function(e){
     var target = $(this).attr('data-target');
     var $e = $('#' + target);
     if($e.is(':visible')){
-      $(this).find('span').html(symbols.show);
+      $(this).find('span.hideable-icon').html(symbols.show);
       $e.hide();
     }else{
-      $(this).find('span').html(symbols.hide);
+      $(this).find('span.hideable-icon').html(symbols.hide);
       $e.show().addClass('flash');
+      lazyLoadIframe($e)
     }
     e.preventDefault();
   })
 
   $('.hideable').click();
+}
+
+
+$(function(){
+  initHideables();
 });
